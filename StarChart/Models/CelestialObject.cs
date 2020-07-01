f5aab7bd57e9b57fc0ce7dbed75529aaa9804612
jbrouns <jbrouns@highwire-it.nl>
2020-07-01 19:17:09 +0200

    chapter 3 take 3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -7,16 +7,16 @@ namespace StarChart.Models
 {
     public class CelestialObject 
     {
-        public int Id {get; set; }
+        public int Id { get; set; }
 
         [Required]
-        public string Name {get; set; }
+        public string Name { get; set; }
 
-        public int? OrbitedObjectId {get; set; }
-        
+        public int? OrbitedObjectId { get; set; }
+
         [NotMapped]
-        public List<CelestialObject> Satellites {get; set; }
+        public List<CelestialObject> Satellites { get; set; }
 
-        public TimeSpan OrbitalPeriod  {get; set; }
+        public TimeSpan OrbitalPeriod  { get; set; }
     }
 }
