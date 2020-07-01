d54a0dd0a798cf7005b7d639260be9a57fdd410b
jbrouns <jbrouns@highwire-it.nl>
2020-07-01 19:15:24 +0200

    chapter 3 take 2

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -12,6 +12,8 @@ namespace StarChart.Models
         [Required]
         public string Name {get; set; }
 
+        public int? OrbitedObjectId {get; set; }
+        
         [NotMapped]
         public List<CelestialObject> Satellites {get; set; }
 
